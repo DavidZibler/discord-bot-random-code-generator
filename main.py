ae348ff4098bc3eb39302e6a3da1010e21487ddc
DavidZibler <strangedave11@gmail.com>
2022-06-12 09:18:03 +0200

    Setup

---
+++ main.py
@@ -0,0 +1,19 @@
+# -----{ Imports requirements }-----
+import discord
+from discord.ext import commands
+import os   # default module
+from dotenv import load_dotenv
+
+
+load_dotenv()   # load all the variables from the env file
+bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    print("---------------------------------------------------")
+    print("RRC - Code Generator, bot is loaded & ready to use!")
+    print("---------------------------------------------------")
+
+
+bot.run(os.getenv('TOKEN'))
